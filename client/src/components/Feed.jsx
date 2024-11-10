adc9b134fb57667e3997338a807a1a3a71d34e80
Kanav Goyal <kanavgoyal898@gmail.com>
2024-11-10 11:12:34 +0530

    Fix Overflows

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -4,12 +4,14 @@ import './Feed.css'
 const Publications = [
     {
         title: "A Comprehensive Review and Analysis of Machine Learning Approaches for Big Data Analytics and Data Mining Techniques in the Context of Cloud Computing",
+        link: "/",
         authors: [
             { profile: "Emily Carter", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" }
         ]
     },
     {
         title: "Exploring Advanced Data Structures, Computational Algorithms, and Their Applications in Bioinformatics for Real-World Healthcare Problems",
+        link: "/",
         authors: [
             { profile: "Michael Johnson", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Sophia Taylor", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" }
@@ -17,6 +19,7 @@ const Publications = [
     },
     {
         title: "Novel Approaches in Graph Neural Networks and Their Applications to Complex Systems: A Detailed Review on Algorithms, Challenges, and Solutions",
+        link: "/",
         authors: [
             { profile: "David Brown", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Olivia Davis", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
@@ -25,6 +28,7 @@ const Publications = [
     },
     {
         title: "Scalable and Efficient Machine Learning Models for High-Dimensional Datasets in Artificial Intelligence and Their Applications to Real-Time Systems",
+        link: "/",
         authors: [
             { profile: "Liam Garcia", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Emma Martinez", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
@@ -34,6 +38,7 @@ const Publications = [
     },
     {
         title: "An In-Depth Exploration of Multi-Agent Systems and Their Application to Cooperative Robotic Solutions in Autonomous Vehicle Technology",
+        link: "/",
         authors: [
             { profile: "Benjamin Lee", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Ava Harris", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
@@ -44,6 +49,7 @@ const Publications = [
     },
     {
         title: "Deep Reinforcement Learning for Complex Task Environments: Exploring Algorithms, Techniques, and Real-Time Decision Making in Autonomous Systems",
+        link: "/",
         authors: [
             { profile: "Mia King", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Alexander Wright", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
@@ -55,6 +61,7 @@ const Publications = [
     },
     {
         title: "An Advanced Study on Quantum Computing Algorithms and Their Applications in Cryptography and Data Security for Modern Computational Systems",
+        link: "/",
         authors: [
             { profile: "Sophia Adams", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Liam Johnson", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" }
@@ -62,6 +69,7 @@ const Publications = [
     },
     {
         title: "Neural Network Architectures for Natural Language Processing and Speech Recognition: A Deep Dive into Modern Approaches and Future Directions",
+        link: "/",
         authors: [
             { profile: "Lucas Thompson", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Emma White", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
@@ -70,6 +78,7 @@ const Publications = [
     },
     {
         title: "A Comparative Analysis of Deep Learning Frameworks and Their Performance Metrics in Large-Scale Image and Video Recognition Systems",
+        link: "/",
         authors: [
             { profile: "Charlotte Harris", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Amelia Davis", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" }
@@ -77,6 +86,7 @@ const Publications = [
     },
     {
         title: "Optimizing Machine Learning Algorithms for Edge Computing in the Internet of Things: A Study on Efficiency, Latency, and Scalability",
+        link: "/",
         authors: [
             { profile: "Lucas Robinson", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
             { profile: "Grace Adams", imageUrl: "https://media.gq.com/photos/5aa72bbf1d388948225ce5cd/4:3/w_1920,h_1440,c_limit/josh-radnor-gq.jpg" },
@@ -117,22 +127,24 @@ const Feed = () => {
                         <table className="publication-table">
                             {Publications.map((publication, index) => {
                                 return (
-                                    <tr key={index} className="publication-row">
-                                        <td className="table-column-serial">{index+1}.</td>
-                                        <td className="table-column-title">{publication.title}</td>
-                                        <td className="table-column-profiles">
-                                            <div className="contributor-list">
-                                                <div className="contributor-pictures">
-                                                    {publication.authors.slice(0, 3).map((author, index) => {
-                                                        return (
-                                                            <img key={index} className="contributor-picture" src={author.imageUrl} alt="contributor" />
-                                                        )
-                                                    })}
-                                                </div>
-                                                {publication.authors.length > 3 && <a className="more-contributors" href="/"> + {publication.authors.length - 3} more</a>}
-                                            </div>
-                                        </td>
-                                    </tr>
+                                    <a href="{publication.link}">
+                                        <tr key={index} className="publication-row">
+                                            <td className="table-column-serial">{index+1}.</td>
+                                            <td className="table-column-title">{publication.title}</td>
+                                            <td className="table-column-profiles">
+                                                <a href="/" className="contributor-list">
+                                                    <div className="contributor-pictures">
+                                                        {publication.authors.slice(0, 3).map((author, index) => {
+                                                            return (
+                                                                <img key={index} className="contributor-picture" style={{ zIndex: publication.authors.length - index }} src={author.imageUrl} alt="contributor" />
+                                                            )
+                                                        })}
+                                                    </div>
+                                                    {publication.authors.length > 3 && <a className="more-contributors" href="/"> + {publication.authors.length - 3} more</a>}
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    </a>
                                 )
                             })}
                         </table>
