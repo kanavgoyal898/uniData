5c09275c89ee749a867653625bfaac309838edd3
Kanav Goyal <kanavgoyal898@gmail.com>
2024-11-15 00:51:00 +0530

    Add Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
@@ -11,7 +12,16 @@ function App() {
 
   return (
     <div className="content">
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<div className="content"><Homepage /></div>} />
+          <Route path="/profile" element={<div className="content"><Profile /></div>} />
+          <Route path="/signin" element={<div className="content"><SignIn /></div>} />
+          <Route path="/signup" element={<div className="content"><SignUp /></div>} />
+          <Route path="/paper" element={<div className="content"><Paper /></div>} />
+          <Route path="/feed" element={<div className="content"><Feed /></div>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
